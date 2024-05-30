bc9020af5ed46f7112634342559b1475f57dadd8
sujan pal <sujanpal79@gmail.com>
2024-05-30 17:51:03 +0530

    Fetch the data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
-export default function Home() {
+const getData = async()=>{
+  const res = await fetch("https://jsonserver.reactbd.com/phone")
+  if(!res.ok){
+    throw new Error("Failed to fetch data")
+  }
+  return res.json();
+}
+export default async function Home() {
+  const products = await getData();
+  console.log(products);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p className="text-center py-10 text-lg">Products will go here</p>
