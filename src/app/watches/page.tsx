9cb407b9437c4ba1b67ee3376cb8640148a46704
sujan pal <sujanpal79@gmail.com>
2024-05-30 20:52:06 +0530

    Create Phone Case & Watches Page

--- src/app/watches/page.tsx
+++ src/app/watches/page.tsx
@@ -2,21 +2,21 @@ import Products from "@/components/Products";
 import Title from "@/components/Title";
 
 const getData = async()=>{
-    const res = await fetch("https://jsonserver.reactbd.com/phonecase")
+    const res = await fetch("https://jsonserver.reactbd.com/watch")
     if(!res.ok){
       throw new Error("Failed to fetch data")
     }
     return res.json();
   };
 
-const Phonecase = async() => {
-    const products = await getData();
+const Watches = async() => {
+  const products = await getData();
     return (
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Title title="Make your phone looks cool with beautiful Phone Case"/>
+        <Title title="Find your favourite Watches"/>
         <Products products = {products}/>
       </div>
     );
 }
 
-export default Phonecase;
+export default Watches
