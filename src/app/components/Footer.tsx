a745b5779aa21ef570af090baba6ef30908c45c4
sujan pal <sujanpal79@gmail.com>
2024-05-30 17:22:04 +0530

    Craete NotFound component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="w-fll h-10 text-sm bg-gray-950 text-gray-200 flex">
-        <p className="text-center">All rights reserved - Developed by Sujan Pal</p>
+    <div className="w-fll h-10 text-sm bg-gray-950 flex flex-col text-gray-200 py-10">
+        <p className="text-center">All rights reserved - Developed by <Link href="https://www.sujanpal.in/" target='newtab'>Sujan Pal</Link></p>
     </div>
   )
 }
